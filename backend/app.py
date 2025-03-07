c202e04b64ed4a0220dc5b554b9b68625a50e3a7
Yue An <annyue@utexas.edu>
2025-03-07 11:10:22 -0600

    connect to mongodb and implement the auth logic

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,32 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["secret_key"] = "ee80e69581676bd475d118d83c238748435e7cbe"
+app.config["MONGO_URI"] = "mongodb+srv://bits8ece461l:SWELab#2025@8bits-461l.hlqqd.mongodb.net/8bit_db?retryWrites=true&w=majority&appName=8bits-461L"
+
+#setup mando db
 CORS(app)
+mongo = PyMongo(app)
+db = mongo.db
+
+@app.route("/test-mongo", methods=["GET"])
+def test_mongo():
+    try:
+        user = db.users.find_one()
+        if user:
+            return jsonify({"message": "MongoDB connected successfully!", "user": str(user)}), 200
+        else:
+            return jsonify({"message": "MongoDB connected, but no users found!"}), 200
+    except Exception as e:
+        return jsonify({"error": "MongoDB connection failed!", "details": str(e)}), 500
 
 @app.route("/login",methods=["POST"])
 def login():
 
     # get the data from the request
     data = request.get_json()
-    print("Parsed JSON data:", data)  # Print the parsed data
 
     # validate the data
     if not data or not data.get('username') or not data.get('password'):
@@ -21,7 +37,15 @@ def login():
 
     print(f"Username: {username}, Password: {password}")  # Print received values
     # check the data
-    return jsonify({"message": "Login successful"}), 200
+    user = db.user.find_one({"username": username})
+    if user:
+        if user['password'] == password:
+            return jsonify({"message": "Login successful"}), 200
+        else:
+            return jsonify({"error": "Invalid password"}), 401
+
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
