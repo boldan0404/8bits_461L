77d303db35c4f2e78eb27ed3e4661a92444bf806
Benjamin Dalton <bendalton6463@gmail.com>
2025-03-07 11:02:10 -0600

    New User

--- frontend/login-app/src/App.js
+++ frontend/login-app/src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login"; // Import your Login component
 import ForgotPassword from "./ForgotPassword";
+import NewUser from "./NewUser"; // Import New User component
 
 function App() {
-  // return <Login />; // Render the Login component
-  return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          {/* Define routes for different views */}
-          <Route path="/" element={<Login />} /> {/* Login Page */}
-          <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Forgot Password Page */}
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="app-container">
+                <Routes>
+                    {/* Define routes for different views */}
+                    <Route path="/" element={<Login />} /> {/* Login Page */}
+                    <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Forgot Password Page */}
+                    <Route path="/new-user" element={<NewUser />} /> {/* New User Registration Page */}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
