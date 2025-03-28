dbe3b2f23ae44fdac20216427b8b56f95b63500c
Benjamin Dalton <bendalton6463@gmail.com>
2025-03-28 10:32:36 -0500

    Fixes

--- frontend/login-app/src/App.js
+++ frontend/login-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login"; // Import your Login component
 import ForgotPassword from "./ForgotPassword";
 import NewUser from "./NewUser"; // Import New User component
+import Projects from "./Projects"
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Route path="/" element={<Login />} /> {/* Login Page */}
                     <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Forgot Password Page */}
                     <Route path="/new-user" element={<NewUser />} /> {/* New User Registration Page */}
+                    <Route path="/projects" element={<Projects />} />  {/* Added route for projects page */}
                 </Routes>
             </div>
         </Router>
