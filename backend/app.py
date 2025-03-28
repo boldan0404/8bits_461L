c14ab68b0f57b600578c3a2e2a5807d30c57d5eb
Yue An <annyue@utexas.edu>
2025-03-28 10:10:05 -0500

    handle password encryption

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager
 
 app = Flask(__name__)
 app.config["secret_key"] = "ee80e69581676bd475d118d83c238748435e7cbe"
-app.config["MONGO_URI"] = "mongodb+srv://bits8ece461l:SWELab#2025@8bits-461l.hlqqd.mongodb.net/8bit_db?retryWrites=true&w=majority&appName=8bits-461L"
+app.config["MONGO_URI"] = "mongodb+srv://bits8ece461l:SWELab#2025@8bits-461l.hlqqd.mongodb.net" \
+"/8bit_db?retryWrites=true&w=majority&appName=8bits-461L"
+app.config["JWT_SECRET_KEY"] = "super-secret-key" 
 
 #setup mando db
 CORS(app)
 mongo = PyMongo(app)
 db = mongo.db
+jwt = JWTManager(app)
 
 @app.route("/test-mongo", methods=["GET"])
 def test_mongo():
@@ -39,11 +44,13 @@ def login():
     # check the data
     user = db.users.find_one({"username": username})
     print(user)
+
     if user:
-        if user['password'] == password:
-            return jsonify({"message": "Login successful"}), 200
+        if check_password_hash(user['password'], password):
+            access_token = create_access_token(identity=username)
+            return jsonify({"message": "Login successful", "token": access_token}), 200
         else:
-            return jsonify({"error": "Invalid password"}), 401
+            return jsonify({"error": "Invalid username or password"}), 401
 
     else:
         return jsonify({"error": "User not found"}), 404
@@ -63,6 +70,9 @@ def register():
     if db.users.find_one({"username":username}):
         return jsonify({"error": "User already exists"}), 401
     
+    # Hash the password
+    password = generate_password_hash(password, method='pbkdf2:sha256')
+
     # insert the user
     db.users.insert_one({"username":
     username, "password": password})
